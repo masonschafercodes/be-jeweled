39036c5a67467d819416be3a7651d6a341dc0a82
masonschafercodes <indytechbox@gmail.com>
2022-08-16 15:44:10 -0400

    feat: create customer

--- src/client.ts
+++ src/client.ts
@@ -1,14 +1,37 @@
 interface IClient {
   stripe_key: string;
+  stripe_api_url: string;
 }
 
 export class BeJeweledClient {
   stripe_key: string;
-  constructor({ stripe_key }: IClient) {
+  stripe_api_url: string;
+  constructor({
+    stripe_key,
+    stripe_api_url = "https://api.stripe.com",
+  }: IClient) {
     this.stripe_key = stripe_key;
+    this.stripe_api_url = stripe_api_url;
   }
 
-  async getStripeKey() {
-    return this.stripe_key;
+  async createCustomer(
+    email: string | null,
+    description: string | null,
+    metadata: Object | null,
+    name: string | null,
+    phone: string | null
+  ): Promise<any> {
+    const response = await fetch(`${this.stripe_api_url}/v1/customers`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${this.stripe_key}`,
+      },
+      body: `email=${email}&description=${description}&metadata=${JSON.stringify(
+        metadata
+      )}&name=${name}&phone=${phone}`,
+    });
+
+    return await response.json();
   }
 }
